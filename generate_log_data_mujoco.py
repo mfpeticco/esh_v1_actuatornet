f1ce745d826aba78c879c1f993696cccf6b073b5
Gabriel Margolis <gmargo@mit.edu>
2024-11-22 17:34:06 -0500

    simulate eyesight hand in mujoco, mass properties are missing

--- generate_log_data_mujoco.py
+++ generate_log_data_mujoco.py
@@ -2,7 +2,8 @@ import mujoco
 import mujoco.viewer
 import numpy as np
 
-model = mujoco.MjModel.from_xml_path(f"assets/go1/xml/go1.xml")
+# model = mujoco.MjModel.from_xml_path(f"assets/go1/xml/go1.xml")
+model = mujoco.MjModel.from_xml_path(f"assets/esh/right_hand_mjcf_torque.xml")
 data = mujoco.MjData(model)
                 
 viewer = mujoco.viewer.launch_passive(model, data)
@@ -12,8 +13,13 @@ viewer._render_every_frame = True
 def apply_command(kp, kd, q_des, dq_des, tau_ff):
     
     for _ in range(10):
-        joint_pos = data.qpos[7:] # Skip the position and rotation of the base
-        joint_vel = data.qvel[6:] # Skip the linear and angular velocity of the base
+        # # Floating base robots
+        # joint_pos = data.qpos[7:] # Skip the position and rotation of the base
+        # joint_vel = data.qvel[6:] # Skip the linear and angular velocity of the base
+
+        # Fixed base robots
+        joint_pos = data.qpos
+        joint_vel = data.qvel
 
         # Apply PD control
         torques = kp * (q_des - joint_pos) + kd * (dq_des - joint_vel) + tau_ff
@@ -24,11 +30,11 @@ def apply_command(kp, kd, q_des, dq_des, tau_ff):
     if viewer is not None:
         viewer.sync()
 
-num_dof = 12
-random_scale = 0.2
+num_dof = 9
+random_scale = 0.0
 
-kp = [20] * num_dof
-kd = [0.5] * num_dof
+kp = [300] * num_dof
+kd = [4] * num_dof
 dq_des = [0] * num_dof
 tau_ff = [0] * num_dof
 
@@ -39,7 +45,7 @@ log_data = {
     "torques": [],
 }
 
-for i in range(1000):
-    q_des = np.random.uniform(-random_scale, random_scale, num_dof)
+while True:
+    q_des = np.random.uniform(-random_scale, random_scale, num_dof) #- 0.5
     apply_command(kp, kd, q_des, dq_des, tau_ff)
     
